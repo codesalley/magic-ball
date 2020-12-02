a867d900a5dd40d77416585e53395c29c9408c2e
Jux salley <juxsalley@Juxs-MacBook-Air.local>
2020-12-02 09:01:31 -0800

    ball challenge done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Center(
+              child: Text('Ask me Anything '),
+            ),
+            backgroundColor: Color(0xff3158AB),
+          ),
+          body: MyApp(),
+          backgroundColor: Colors.blueAccent,
+        ),
       ),
     );
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int ballNum = 0;
+  void shuffle() {
+    ballNum = Random().nextInt(4);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          shuffle();
+        });
+      },
+      child: Center(
+        child: Container(
+          padding: EdgeInsets.all(15),
+          color: Colors.blueAccent,
+          child: Image(
+            image: AssetImage('images/ball${ballNum + 1}.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
